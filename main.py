b6636c3c4b8d335dd5d388ae7b4cc38dc114d924
FrankieJesse <98450396+FrankieJesse@users.noreply.github.com>
2022-01-29 14:58:01 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import intrestpython.intrestpython
+if __name__ == '__main__':
+    print("TestForIntrestPythonBegin")
+    intrestpython.intrestpython.IntrestPythonMain()
+    print("TestForIntrestPythonEnd")
+    inputChar = input("Are you want to exit ? enter 'Y' to exit or 'N' to continue: ")
+    while True :
+        if inputChar == 'Y' or inputChar == 'y' :
+            print("exit your system")
+            break
